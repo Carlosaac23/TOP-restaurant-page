774c936154e5049154c731210906b9b0c3541f49
Carlos <carlosaac232001@gmail.com>
2025-09-25 19:50:25 -0500

    feat: add new page to last button and render home page by default

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import home from './home';
 import about from './about';
+import contact from './contact';
+
+home();
 
 const buttons = document.querySelectorAll('button');
 buttons.forEach(button => {
@@ -7,5 +10,7 @@ buttons.forEach(button => {
     button.addEventListener('click', home);
   } else if (button.textContent === 'About') {
     button.addEventListener('click', about);
+  } else {
+    button.addEventListener('click', contact);
   }
 });
