955c774e8fc3d5024d7e062ccdc69b98a0de3dbb
Carlos <carlosaac232001@gmail.com>
2025-09-25 21:13:32 -0500

    feat: create whole contact page with simple form

--- src/contact.js
+++ src/contact.js
@@ -6,9 +6,31 @@ export default function contact() {
     container.removeChild(container.firstChild);
   }
 
-  const title = document.createElement('h1');
+  const contactTitle = document.createElement('h1');
+  contactTitle.classList.add('contact__title');
+  contactTitle.textContent = 'Contact';
 
-  title.textContent = 'Desde Contact';
+  const contactForm = document.createElement('div');
+  contactForm.classList.add('contact__form');
+  contactForm.innerHTML = `
+    <form class="form">
+      <div>
+        <label for="name">Name</label>
+        <input type="text" name="name" id="name" />
+      </div>
 
-  container.appendChild(title);
+      <div>
+        <label for="email">Email</label>
+        <input type="text" name="email" id="email" />
+      </div>
+
+      <div>
+        <label for="message">Message</label>
+        <textarea rows="6"></textarea>
+      </div>
+    </form>
+  `;
+
+  container.appendChild(contactTitle);
+  container.appendChild(contactForm);
 }
